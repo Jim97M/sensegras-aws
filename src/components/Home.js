00176adcbbb9c803ab72a88ad116550a1a43bc67
Jim97M <wabuyajames@gmail.com>
2022-06-24 22:55:14 +0300

    Data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,34 @@
 import React from "react";
 import "./home.css";
-
+import { farmItems } from "../data";
 const Home = () => {
   return (
     <div className="WeatherBoards">
       <div className="WeatherLeft-board">
           <h3>Sensor</h3>
-      <div className="WeatherCard-detail">
-           <div>
+      <div className="WeatherCard-detail-top">
+           <div className="start">
+               <h4>Sensor Location</h4>
+               <p>SensorLocation</p>
+           </div>
+           <div className="center">
+               <h4>Sensor Location</h4>
+               <p>SensorLocation</p>
+           </div>
+           <div className="end">
                <h4>Sensor Location</h4>
                <p>SensorLocation</p>
            </div>
       </div>
-      <div className="WeatherCard-detail">
-           <div>
-               <h4>Sensor Location</h4>
-               <p>SensorLocation</p>
-           </div>
+      <div className="data-item">
+
       </div>
-      <div className="WeatherCard-detail">
-           <div>
-               <h4>Sensor Location</h4>
-               <p>SensorLocation</p>
-           </div>
-      </div>
+      
       </div>
       <div className="WeatherRight-board">
+        <div className="WeatherCard-detail">
+         <h2>Left Card</h2>
+        </div>
         
       </div>
     </div>
---
+++ src/data.js
@@ -0,0 +1,38 @@
+ export const farmItems = [
+    {
+        "name": "Cuz Aviation",
+        "unit": "mg/L",
+        "number": 36,
+        "comment": "IDEAL RANGE",
+        "range": "80-100"
+    },
+    {
+        "name": "Light",
+        "unit": "Lux",
+        "number": 82,
+        "comment": "IDEAL RANGE",
+        "range": "80-100"
+    },
+    {
+        "name": "Soil_Moisture",
+        "unit": "%",
+        "number": 98,
+        "comment": "IDEAL RANGE",
+        "range": "80-100"
+    },
+    {
+        "name": "Soil Temp",
+        "unit": "mg/L",
+        "number": 35,
+        "comment": "IDEAL RANGE",
+        "range": "80-100"
+    },    
+    {
+        "name": "Salinity",
+        "unit": "dS/M",
+        "number": 43,
+        "comment": "IDEAL RANGE",
+        "range": "80-100"
+    }      
+ ]
+ 
