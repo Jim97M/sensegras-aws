6096254a5a42d8a9d4979821eab844cdfff3e87c
Jim97M <wabuyajames@gmail.com>
2022-06-25 23:55:32 +0300

    Styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,81 @@
 import React from "react";
 import "./home.css";
 import { farmItems } from "../data";
+import top from "./top.png";
+import sun from "./sun.png";
 const Home = () => {
   return (
-    <div className="WeatherBoards">
+    <div className="background">   
+       <div className="WeatherBoards">
       <div className="WeatherLeft-board">
-          <h3>Sensor</h3>
-      <div className="WeatherCard-detail-top">
+          {/* <h3>Sensor</h3> */}
+      <div className="weather-detail-top">
            <div className="start">
-               <h4>Sensor Location</h4>
-               <p>SensorLocation</p>
+               <h4>Basic Sensor</h4>
+               <p>Sensor Location LAT | Long</p>
            </div>
            <div className="center">
-               <h4>Sensor Location</h4>
-               <p>SensorLocation</p>
+               <h5>22.12.2022| 11.40 PM</h5>
            </div>
            <div className="end">
-               <h4>Sensor Location</h4>
-               <p>SensorLocation</p>
+               <button>ADD SENSOR</button>
            </div>
       </div>
       <div className="data-item">
-
+          {farmItems.map((farm) => {
+            return(
+              <div className="name">
+              <p>
+                {farm.name} 
+                </p>
+                <p className="unit">
+                  {farm.unit}
+                </p>
+                <p className="number">
+                  {farm.number}
+                </p>
+                 <div className="data bar">
+                    
+                  </div>
+                <p className="range">
+                  {farm.comment}
+                </p>
+                
+                </div>
+                
+            )
+                 
+          })}
+          <div className="vertical"> </div>
+          <div className="data-right">
+             <img src={top} />
+             <p>Status Online</p>
+             <p>Battery Status: 92%</p>
+             <p>Mac ID: 1102100</p>
+             <p><button> VIEW GRAPHS</button> </p>
+             <p><button> DOWNLOAD REPORT</button></p>
+          </div>
+          
       </div>
-      
+       
       </div>
       <div className="WeatherRight-board">
-        <div className="WeatherCard-detail">
-         <h2>Left Card</h2>
+        {/* <h3>Weather</h3> */}
+        <div className="card-header">
+          <img className="sun" src={sun} />
+          <h3 className="temp-head">80F</h3>
         </div>
-        
+         <div className="bottom-part">
+           <p>Precipitation Intensity: 81</p>
+           <p>Precipitation Probability: 81</p>
+           <p>Pressure: 81</p>
+           <p>Wind Speed: 4.87kmph</p>
+           <p>Wind Direction: 40.8N</p>
+         </div>
       </div>
     </div>
+    </div>
+
   )
 }
 
--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@
         "name": "Cuz Aviation",
         "unit": "mg/L",
         "number": 36,
+        "len": 40,
         "comment": "IDEAL RANGE",
         "range": "80-100"
     },
@@ -10,6 +11,7 @@
         "name": "Light",
         "unit": "Lux",
         "number": 82,
+        "len": 80,
         "comment": "IDEAL RANGE",
         "range": "80-100"
     },
@@ -17,6 +19,7 @@
         "name": "Soil_Moisture",
         "unit": "%",
         "number": 98,
+        "len": 40,
         "comment": "IDEAL RANGE",
         "range": "80-100"
     },
@@ -24,6 +27,7 @@
         "name": "Soil Temp",
         "unit": "mg/L",
         "number": 35,
+        "len": 30,
         "comment": "IDEAL RANGE",
         "range": "80-100"
     },    
@@ -31,8 +35,25 @@
         "name": "Salinity",
         "unit": "dS/M",
         "number": 43,
+        "len": 90,
         "comment": "IDEAL RANGE",
         "range": "80-100"
-    }      
+    },
+    {
+        "name": "Salinity",
+        "unit": "dS/M",
+        "number": 43,
+        "len": 90,
+        "comment": "IDEAL RANGE",
+        "range": "80-100"
+    },
+    {
+        "name": "Salinity",
+        "unit": "dS/M",
+        "number": 43,
+        "len": 90,
+        "comment": "IDEAL RANGE",
+        "range": "80-100"
+    },
  ]
  
