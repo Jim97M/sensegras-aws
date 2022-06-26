cfc390f243d3d52ba2ea6663a0597282dd460d10
Jim97M <wabuyajames@gmail.com>
2022-06-26 17:59:46 +0300

    Frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,19 @@ import sun from "./sun.png";
 const Home = () => {
   return (
     <div className="background">   
+     <div className="navbar">
+           <p>SenseGrass</p>
+           <p>File</p>
+           <p>View</p>
+           <p>Map</p>
+           <p>Help</p>
+           <p>10 Jun 2021</p>
+           <p>Lalit Gautam</p>
+        </div>
        <div className="WeatherBoards">
+      
       <div className="WeatherLeft-board">
+      
           {/* <h3>Sensor</h3> */}
       <div className="weather-detail-top">
            <div className="start">
@@ -46,18 +57,19 @@ const Home = () => {
             )
                  
           })}
+
+      
           <div className="vertical"> </div>
           <div className="data-right">
              <img src={top} />
              <p>Status Online</p>
              <p>Battery Status: 92%</p>
              <p>Mac ID: 1102100</p>
-             <p><button> VIEW GRAPHS</button> </p>
-             <p><button> DOWNLOAD REPORT</button></p>
+             <button> VIEW GRAPHS</button> 
+             <button> DOWNLOAD REPORT</button>
           </div>
           
-      </div>
-       
+          </div>
       </div>
       <div className="WeatherRight-board">
         {/* <h3>Weather</h3> */}
@@ -72,6 +84,17 @@ const Home = () => {
            <p>Wind Speed: 4.87kmph</p>
            <p>Wind Direction: 40.8N</p>
          </div>
+      </div>
+    </div>
+    <div className="bottom-data">
+      <div className="init-name">
+       <p className="fa fa-plus">Add Activity</p>
+      </div>
+      <div className="end-data">
+        <div className="init-name">
+          <p>Ask Sana</p>          
+        </div> 
+         
       </div>
     </div>
     </div>
